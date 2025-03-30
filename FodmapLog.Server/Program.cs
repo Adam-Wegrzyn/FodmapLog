b525c97f475833da85ce3389de3599f6495eab9b
Adam <adamewgrzyn.it@gmail.com>
2025-03-30 15:41:30 +0200

    added test config

--- FodmapLog.Server/Program.cs
+++ FodmapLog.Server/Program.cs
@@ -5,12 +5,27 @@ using DataAccess.Interfaces;
 using DataAccess.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Identity.Web;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Azure.Identity;
+using System.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["ApplicationInsights:InstrumentationKey"]);
+builder.Services.AddApplicationInsightsTelemetry(options =>
+{
+    options.ConnectionString = builder.Configuration["ApplicationInsights:ConnectionString"];
+});
 
+var keyVaultName = builder.Configuration["KeyVaultName"];
+if (!string.IsNullOrEmpty(keyVaultName))
+{
+    var keyVaultUri = new Uri($"https://{keyVaultName}.vault.azure.net/");
+    builder.Configuration.AddAzureKeyVault(keyVaultUri, new DefaultAzureCredential());
+}
 
 // Add services to the container.
 builder.Services.AddCors(options =>
@@ -18,16 +33,80 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
                builder =>
                {
-            builder.WithOrigins("http://localhost:4200")
-                                .AllowAnyHeader()
-                                .AllowAnyMethod()
-                                .AllowAnyOrigin();
-        });
+                   builder.WithOrigins("http://localhost:4200")
+                                       .AllowAnyHeader()
+                                       .AllowAnyMethod()
+                                       .AllowAnyOrigin();
+               });
 });
 builder.Services.AddControllers();
+
+builder.Services.AddLogging(logging =>
+{
+    logging.AddConsole();
+    logging.AddDebug();
+    logging.SetMinimumLevel(LogLevel.Debug);
+    logging.AddFilter("Microsoft.AspNetCore.Authentication", LogLevel.Debug);
+});
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(options =>
+    {
+        builder.Configuration.Bind("AzureAd", options);
+        options.Audience = builder.Configuration["AzureAd:Audience"];
+        options.Authority = builder.Configuration["AzureAd:Authority"];
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            //ValidateIssuer = false, // Turn off issuer validation
+            //ValidateAudience = false, // Turn off audience validation
+            //ValidateLifetime = true, // Optionally, you can turn this off as well
+            //ValidateIssuerSigningKey = true, // Optionally, you can turn this off as well
+            //RoleClaimType = "roles", // Specify the claim type for roles
+            //NameClaimType = "name"
+        };
+    }, options =>
+    {
+        builder.Configuration.Bind("AzureAd", options);
+        options.ClientSecret = builder.Configuration["AzureAd--ClientSecret"];
+        Console.WriteLine(builder.Configuration["AzureAd--ClientSecret"]);
+    })
+    .EnableTokenAcquisitionToCallDownstreamApi(options =>
+    {
+        builder.Configuration.Bind("AzureAd", options);
+    })
+    .AddInMemoryTokenCaches();
+
+builder.Services.AddAuthorization();
+var test = builder.Configuration["AzureAd--ClientSecret"];
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer"
+       
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 builder.Services.AddScoped<IFodmapLogRepository, FodmapLogRepository>();
 builder.Services.AddHttpClient<IProductsApiService, ProductApiService>();
@@ -38,7 +117,7 @@ builder.Services.AddDbContext<FodmapLogDbContext>(options =>
 {
     if (builder.Environment.IsDevelopment())
     {
-        options.UseSqlServer(builder.Configuration.GetConnectionString("prodConnection"));
+        options.UseSqlServer(builder.Configuration.GetConnectionString("devConnection"));
     }
     else
     {
@@ -64,11 +143,12 @@ app.UseCors();
 //    app.UseSwaggerUI();
 //}
 
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
